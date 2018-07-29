95ce7ce29fd19c9d9c6894b7d9db8bb6b468c8fd
Tonet Jallo <tonet666p@gmail.com>
2018-07-29 00:59:56 -0500

    Simplified the code.

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -10,10 +10,30 @@
 
 String rx_data = "";
 char character;
-int sw_status[] = {0,0,0};
+bool sw_status[] = {0,0,0};
+String sw_values[8]={"000","001","010","011","100","101","110","111"};
 int signal = 0;
+int code = 0;
+
+/*
+ *   000 = 0
+ *   001 = 1
+ *   010 = 2
+ *   011 = 3
+ *   100 = 4
+ *   101 = 5
+ *   110 = 6
+ *   111 = 7
+ *   
+ */
 
 SoftwareSerial HC12(8,9);
+
+void set_sw_pattern(){
+  sw_status[0]=sw_values[code].substring(0,1).toInt();
+  sw_status[1]=sw_values[code].substring(1,2).toInt();
+  sw_status[2]=sw_values[code].substring(2).toInt();
+}
 
 void setup(){
   pinMode(OUT0, OUTPUT);
@@ -26,34 +46,16 @@ void setup(){
 char led;
 
 void loop(){
-
-  while(HC12.available()>0){
-    character = HC12.read();
-    if (character == ' ') {
-      if (rx_data.length() == 7) {
-        
-        if ( rx_data[0] == '{' && rx_data[6]=='}' ){
-          sw_status[0]=rx_data.substring(1,2).toInt();
-          sw_status[1]=rx_data.substring(3,4).toInt();
-          sw_status[2]=rx_data.substring(5,6).toInt();
-
-          //Serial.println("Received str:("+String(sw_status[0])+"-"+String(sw_status[1])+"-"+String(sw_status[2])+")");
-          //HC12.println("Received str:("+String(sw_status[0])+"-"+String(sw_status[1])+"-"+String(sw_status[2])+")");
-          
-          signal = 10;
-        }
-      }
-      rx_data = "";
-    }else{
-      rx_data += character;
-    }
-  }
-  if(HC12.available()<1){
+  if(HC12.available()>0){
+    rx_data = HC12.read();
+    code = rx_data.toInt() - 48;
+    set_sw_pattern();
+    signal = 10;
+    Serial.println(code);
+  }else if(HC12.available()<1){
     if(signal < 5){
-      //Serial.println("Received str:("+String(sw_status[0])+"-"+String(sw_status[1])+"-"+String(sw_status[2])+")");
-      sw_status[0]=0;
-      sw_status[1]=0;
-      sw_status[2]=0;
+      code = 0;
+      set_sw_pattern();
     }
     if (signal > 0){
       signal -= 1;
@@ -63,5 +65,4 @@ void loop(){
   digitalWrite(OUT0,sw_status[0]);
   digitalWrite(OUT1,sw_status[1]);
   digitalWrite(OUT2,sw_status[2]);
-  
 }
--- remote_control/remote_control.ino
+++ remote_control/remote_control.ino
@@ -7,8 +7,23 @@
 #define SW1 A1
 #define SW2 A2
 
-int last_status[3] = {1,1,1};
-int sw_status[3]={0,0,0};
+bool sw_status[3]={0,0,0};
+bool led_status[3]={false,false,false};
+String sw_values[8]={"000","001","010","011","100","101","110","111"};
+String pattern = "000";
+int code = 0;
+
+/*
+ *   000 = 0
+ *   001 = 1
+ *   010 = 2
+ *   011 = 3
+ *   100 = 4
+ *   101 = 5
+ *   110 = 6
+ *   111 = 7
+ *   
+ */
 
 SoftwareSerial HC12(8,9);
 
@@ -24,27 +39,43 @@ void setup() {
   Serial.begin(9600);
 }
 
+
+void switch_pattern(){
+  if(sw_status[0] == 0){
+    led_status[0] = !led_status[0];
+  }
+  if(sw_status[1] == 0){
+    led_status[1] = !led_status[1];
+  }
+  if(sw_status[2] == 0){
+    led_status[2] = !led_status[2];
+  }
+  delay(250);
+}
+
+
 void loop() {
-
   sw_status[0] = digitalRead(SW0);
-  if (sw_status[0] == HIGH && last_status[0] == LOW) {
-     digitalWrite(LED0, !digitalRead(LED0));
-  }
-  last_status[0] = sw_status[0];
-
   sw_status[1] = digitalRead(SW1);
-  if (sw_status[1] == HIGH && last_status[1] == LOW) {
-     digitalWrite(LED1, !digitalRead(LED1));
-  }
-  last_status[1] = sw_status[1];
-
   sw_status[2] = digitalRead(SW2);
-  if (sw_status[2] == HIGH && last_status[2] == LOW) {
-     digitalWrite(LED2, !digitalRead(LED2));
+
+  if(sw_status[0] == 0 || sw_status[1] == 0 || sw_status[2] == 0){
+    switch_pattern();
   }
-  last_status[2] = sw_status[2];
-  delay(50);
+  digitalWrite(LED0,led_status[0]);
+  digitalWrite(LED1,led_status[1]);
+  digitalWrite(LED2,led_status[2]);
+
+  pattern = String(led_status[0])+String(led_status[1])+String(led_status[2]);
+  for(code=0; code<8 ; code++){
+    if(sw_values[code] == pattern){
+      break;
+    }
+  }
+
+  Serial.print(String(sw_status[0])+String(sw_status[1])+String(sw_status[2]));
+  Serial.print("-");
+  Serial.println(code);
+  HC12.print(code);
   
-  //Serial.println("Transmitting str:("+String(digitalRead(LED0))+"-"+String(digitalRead(LED1))+"-"+String(digitalRead(LED2))+")");
-  HC12.print("{"+String(digitalRead(LED0))+","+String(digitalRead(LED1))+","+String(digitalRead(LED2))+"} ");
 }
